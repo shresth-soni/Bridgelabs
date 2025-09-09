f0b807eea06acd9b7f52767d8bdf69e6e0ebd5b5
shresth-soni <shresthsoni0417@gmail.com>
2025-09-09 17:52:59 +0530

    Moved Java files into assignment_3 folder

---
+++ assignment_3/PalindromeChecker.java
@@ -0,0 +1,22 @@
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        String input = "madam";
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
